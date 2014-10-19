bd9d6c2a19156a61135907d257ca88feb76363bb
Jimmy <visperc@gmail.com>
2014-10-19 10:44:22 +0800

    Add xcode project

---
+++ Supernova/Supernova/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Supernova
+//
+//  Created by Jimmy on 10/19/14.
+//  Copyright (c) 2014 Jimmy. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
