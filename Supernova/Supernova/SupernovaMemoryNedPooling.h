bb46387e4d34dc2020c5c0634d0bc5225d6079d6
Jimmy <visperc@gmail.com>
2015-08-19 11:50:43 +0800

    Add align policy

--- Supernova/Supernova/SupernovaMemoryNedPooling.h
+++ Supernova/Supernova/SupernovaMemoryNedPooling.h
@@ -17,8 +17,36 @@ namespace Supernova
     public:
         static void* allocBytes(size_t count,const char* file , int line ,const char* func);
         static void deallocBytes(size_t align ,size_t count , const char* file , int line , const char* func );
+        static void* allocBytesAligned(size_t align ,size_t count , const char* file , int line , const char* func);
         static void deallocBytesAligned(size_t align , void* ptr);
     };
+    
+    class NedPoolingPolicy
+    {
+    public:
+        static inline void* allocateBytes(size_t count, const char* file = 0 ,int line = 0,const char* func = 0)
+        {
+            return NedPoolingImpl::allocBytes(count , file, line, func);
+        }
+        static inline void* deallocateBytes(void* ptr)
+        {
+            return NedPoolingImpl::deallocBytes(ptr);
+        }
+        static inline size_t getMaxAllocationSize()
+        {
+            return std::numeric_limits<size_t>::max();
+        }
+    private:
+        NedPoolingPolicy()
+        {}
+    };
+    
+    template <size_t Alignment = 0>
+    class NedPoolingAlignedPolicy
+    {
+    public:
+        typedef int IsValidAlignment [Alignment <= 128 && ((Alignment & (Alignment-1)) ==0 )? +1 : -1]
+    }
 }
 
 
